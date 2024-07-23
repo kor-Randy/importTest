51b5dac9b39f74c6f3483fd7c4e5a2f14abb103e
tree.ji <tree.ji@kakaocorp.com>
2024-07-23 10:20:49 +0900

    Middle implement

--- Sources/Middle/Middle.swift
+++ Sources/Middle/Middle.swift
@@ -5,3 +5,30 @@
 @_implementationOnly import End_B
 import End_C
 
+public class Middle {
+    let a = PublicClass_A()
+    let b = PublicClass_B()
+    let c = PublicClass_C()
+    
+    func returnA() -> Struct_A {
+        return a.returnFunc()
+    }
+    
+    func returnB() -> Struct_B {
+        return b.returnFunc()
+    }
+    
+    func returnC() -> Struct_C {
+        return c.returnFunc()
+    }
+    
+    func printA() {
+        a.publicFunc()
+    }
+    func printB() {
+        b.publicFunc()
+    }
+    func printC() {
+        c.publicFunc()
+    }
+}
